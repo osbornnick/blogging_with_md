50225ab08ec7559e1a2f176cce821d97e0726297
osbornnick <nicholas.p.osborn@gmail.com>
2019-11-07 15:19:23 -0500

    post model v1

--- md_blog/models.py
+++ md_blog/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+import markdown
 
-# Create your models here.
-class Pst
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    md_path = models.CharField(max_length=255)
+    html = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+    def save(self, *args, **kwargs):
+        file = open(self.md_path, mode='r')
+        self.html = markdown.markdown(file.read())
+        file.close()
+        super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
--- md_blog/views.py
+++ md_blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from md_blog.models import Post
 
-# Create your views here.
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {"posts": posts}
+    return render(request, "blog_index.html", context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {'post': post}
+    return render(request, "blog_detail.html", context)
